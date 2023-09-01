d9c82e1c2fac2aadca59fbc2c9216d1ce1231de8
Eduardo Guedes Pereira Silva <eduardoguedespsn@gmail.com>
2023-08-31 21:58:48 -0300

    migrations

---
+++ src/Ipet.MVC/Migrations/20230831200719_identityextension2.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Ipet.MVC.Migrations
+{
+    public partial class identityextension2 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Cnpj",
+                table: "AspNetUsers",
+                newName: "Documento");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Documento",
+                table: "AspNetUsers",
+                newName: "Cnpj");
+        }
+    }
+}
