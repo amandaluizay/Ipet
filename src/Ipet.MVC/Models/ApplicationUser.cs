f297f9eea1201dde85690ea92cd33ff21b944100
amanda <luiza.amandasilvah@gmail.com>
2023-08-31 21:58:13 -0300

    fx

---
+++ src/Ipet.MVC/Models/ApplicationUser.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace Ipet.MVC.Models
+{
+    public class ApplicationUser : IdentityUser
+    {
+        public string Nome { get; set; }    
+        public string Password { get; set; }
+        public string Endereco { get; set; }
+        public string ImagemUpload { get; set; }
+        public string Imagem { get; set; }
+        public string Documento { get; set; }
+        public string ConfirmPassword { get; set; }
+    }
+}
