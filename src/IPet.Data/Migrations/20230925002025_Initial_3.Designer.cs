a604dc4f5075f713b9128b50ce9f97dd4689129e
EduardoGuedes06 <eduardoguedespsn@gmail.com>
2023-09-24 21:21:57 -0300

    dd

--- src/IPet.Data/Migrations/20230925002025_Initial_3.Designer.cs
+++ src/IPet.Data/Migrations/20230925002025_Initial_3.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using EnterpriseStore.Data.Context;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,10 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Ipet.Data.Migrations
 {
     [DbContext(typeof(MeuDbContext))]
-    partial class MeuDbContextModelSnapshot : ModelSnapshot
+    [Migration("20230925002025_Initial_3")]
+    partial class Initial_3
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ src/IPet.Data/Migrations/20230925002025_Initial_3.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Ipet.Data.Migrations
+{
+    public partial class Initial_3 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Imagem",
+                table: "Servicos",
+                type: "LONGTEXT",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "varchar(100)")
+                .Annotation("MySql:CharSet", "utf8mb4")
+                .OldAnnotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "Carrinho",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "char(36)", nullable: false, collation: "ascii_general_ci"),
+                    UsuarioId = table.Column<Guid>(type: "char(36)", nullable: false, collation: "ascii_general_ci"),
+                    DataCadastro = table.Column<DateTime>(type: "datetime(6)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Carrinho", x => x.Id);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "CarrinhoProduto",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "char(36)", nullable: false, collation: "ascii_general_ci"),
+                    CarrinhoId = table.Column<Guid>(type: "char(36)", nullable: false, collation: "ascii_general_ci"),
+                    ProdutoId = table.Column<Guid>(type: "char(36)", nullable: false, collation: "ascii_general_ci"),
+                    Quantidade = table.Column<int>(type: "int", nullable: false),
+                    DataCadastro = table.Column<DateTime>(type: "datetime(6)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CarrinhoProduto", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_CarrinhoProduto_Carrinho_CarrinhoId",
+                        column: x => x.CarrinhoId,
+                        principalTable: "Carrinho",
+                        principalColumn: "Id");
+                    table.ForeignKey(
+                        name: "FK_CarrinhoProduto_Produtos_ProdutoId",
+                        column: x => x.ProdutoId,
+                        principalTable: "Produtos",
+                        principalColumn: "Id");
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_CarrinhoProduto_CarrinhoId",
+                table: "CarrinhoProduto",
+                column: "CarrinhoId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_CarrinhoProduto_ProdutoId",
+                table: "CarrinhoProduto",
+                column: "ProdutoId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "CarrinhoProduto");
+
+            migrationBuilder.DropTable(
+                name: "Carrinho");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Imagem",
+                table: "Servicos",
+                type: "varchar(100)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "LONGTEXT")
+                .Annotation("MySql:CharSet", "utf8mb4")
+                .OldAnnotation("MySql:CharSet", "utf8mb4");
+        }
+    }
+}
