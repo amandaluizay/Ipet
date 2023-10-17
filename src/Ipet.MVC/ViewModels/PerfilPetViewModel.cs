5ed75bce1e6ded85c140387708c1880411753dff
EduardoGuedes06 <eduardoguedespsn@gmail.com>
2023-10-17 18:57:08 -0300

    Perfil Pet Repo Montado

---
+++ src/Ipet.MVC/ViewModels/PerfilPetViewModel.cs
@@ -0,0 +1,22 @@
+﻿namespace Ipet.Domain.Models
+{
+    public class PerfilPetViewModel
+    {
+        public Guid Id { get; set; }
+        public Guid IdUsuario { get; set; }
+        public string TipoAnimal { get; set; }
+
+        public string Nome { get; set; }
+
+        public string Raca { get; set; }
+
+        public int Idade { get; set; }
+
+        public string Porte { get; set; }
+
+        public string Imagem { get; set; }
+
+        public string Observacao { get; set; }
+    }
+
+}
